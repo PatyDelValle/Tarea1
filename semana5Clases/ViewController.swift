60bb8066bc1a18a92fc3fbd7a2fecc2d764d0da6
patricia <patricia@macbook-20.local>
2019-03-01 16:24:01 -0600

    Initial Commit

---
+++ semana5Clases/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  semana5Clases
+//
+//  Created by patricia on 3/1/19.
+//  Copyright © 2019 patricia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
